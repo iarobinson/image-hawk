39d2065d733e0ceb17c70fdc31de3cf3d4f84d31
Ian Robinson <ian@ianrobinson.net>
2021-11-27 19:11:11 -0600

    Send functional URLs  (cancel and success) to stripe

--- app/controllers/sessions/charges_controller.rb
+++ app/controllers/sessions/charges_controller.rb
@@ -11,39 +11,36 @@ class Sessions::ChargesController < ApplicationController
   end
 
   def new
-    successful_charge_url = Rails.root.to_s + session_charges_path(@session)
-    cancel_charge_url =
     @charge = Charge.new
     @attachment = ActiveStorage::Attachment.find(params[:image])
   end
 
   def create
     @charge = Charge.new(charge_params)
-    @charge.success_url = "This is the link to the correct place to redirect people who have paid"
-    @charge.cancel_url = session_path(@session)
 
-    stripe_session = Stripe::Checkout::Session.create({
-      line_items: [{
-                     price_data: {
-                       currency: 'usd',
-                       product_data: {
-                         name: "Download the high resolution photo",
-                       },
-                       unit_amount: @session.default_price_cents,
-                     },
-                     quantity: 1,
-                   }],
-      mode: 'payment',
-      success_url: @charge.success_url,
-      cancel_url: @charge.cancel_url,
-    })
+    if @charge.save
+      the_root_url = URI.join(root_url).to_s.chomp('/')
+      @charge.success_url = the_root_url + session_charge_path(@session, @charge)
+      @charge.cancel_url = the_root_url + session_path(@session)
 
-    respond_to do |format|
-      if @session.save
-        redirect_to stripe_session.url
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+      stripe_session = Stripe::Checkout::Session.create({
+        line_items: [{
+           price_data: {
+             currency: 'usd',
+             product_data: {
+               name: "Download the high resolution photo",
+             },
+             unit_amount: @session.default_price_cents,
+           },
+           quantity: 1,
+        }],
+        mode: 'payment',
+        success_url: @charge.success_url,
+        cancel_url: @charge.cancel_url,
+      })
+      redirect_to stripe_session.url
+    else
+      redirect_to @session, notice: "Sorry, something went wrong."
     end
   end
 
@@ -51,6 +48,10 @@ class Sessions::ChargesController < ApplicationController
 
     def set_session
       @session = Session.find(params[:session_id])
+    end
+
+    def set_charge
+      @charge = Charge.find(params[:id])
     end
 
     def charge_params
