80ecfcc3a9bce1eb4f3a3969aa9bb1137a187c83
Ian Robinson <ian@ianrobinson.net>
2024-02-21 13:57:43 -0500

    Add a spots and a spot seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_26_152420) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_21_183917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -158,6 +158,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_26_152420) do
     t.datetime "updated_at", null: false
     t.integer "default_price_cents", default: 0, null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "spots", force: :cascade do |t|
+    t.string "name"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seeds.rb
+
+Spot.create([
+  { name: "Pipeline (Banzai Pipeline)", latitude: 21.6610, longitude: -158.0560 },
+  { name: "Jeffreys Bay (J-Bay)", latitude: -34.0476, longitude: 24.9211 },
+  { name: "Teahupo'o", latitude: -17.8482, longitude: -149.2688 },
+  { name: "Uluwatu", latitude: -8.8245, longitude: 115.1015 },
+  { name: "Cloudbreak", latitude: -17.8795, longitude: 177.1908 },
+  { name: "Snapper Rocks", latitude: -28.1075, longitude: 153.4844 },
+  { name: "Hossegor", latitude: 43.6609, longitude: -1.4438 },
+  { name: "Bells Beach", latitude: -38.3652, longitude: 144.2845 },
+  { name: "Mavericks", latitude: 37.4947, longitude: -122.5005 },
+  { name: "Rincon", latitude: 34.3705, longitude: -119.4359 },
+])
